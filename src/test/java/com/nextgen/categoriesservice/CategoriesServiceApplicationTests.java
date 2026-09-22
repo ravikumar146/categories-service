96ea90a51789b15e4f4be485c782151437d19d70
ravikumar146 <botcha.rav146@gmail.com>
2026-09-22 23:27:58 +0530

    Update CategoriesServiceApplicationTests.java

--- src/test/java/com/nextgen/categoriesservice/CategoriesServiceApplicationTests.java
+++ src/test/java/com/nextgen/categoriesservice/CategoriesServiceApplicationTests.java
@@ -3,9 +3,11 @@ package com.nextgen.categoriesservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = {
-        "spring.config.import="
-})
+@SpringBootTest(
+        properties = {
+                "spring.config.import=optional:"
+        }
+)
 class CategoriesServiceApplicationTests {
 
     @Test
