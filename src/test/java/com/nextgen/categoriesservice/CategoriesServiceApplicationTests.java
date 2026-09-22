a7f3336a6796850dc4b05bba5d589de7319b0e09
Ravi Kumar <botcha.rav146@gmail.com>
2026-09-22 23:23:44 +0530

    Updated gcp secrets to the project and added data base connection details

--- src/test/java/com/nextgen/categoriesservice/CategoriesServiceApplicationTests.java
+++ src/test/java/com/nextgen/categoriesservice/CategoriesServiceApplicationTests.java
@@ -1,18 +1,9 @@
 package com.nextgen.categoriesservice;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
-@EnableAutoConfiguration(exclude = {
-        DataSourceAutoConfiguration.class,
-        HibernateJpaAutoConfiguration.class
-})
-@TestPropertySource(properties = {
+@SpringBootTest(properties = {
         "spring.config.import="
 })
 class CategoriesServiceApplicationTests {
