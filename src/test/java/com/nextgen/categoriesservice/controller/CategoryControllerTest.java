aaeb9917fe5893d8a33e42402cd92431147d5a33
Ravi Kumar <botcha.rav146@gmail.com>
2026-09-22 23:55:42 +0530

    Updated test files

--- src/test/java/com/nextgen/categoriesservice/controller/CategoryControllerTest.java
+++ src/test/java/com/nextgen/categoriesservice/controller/CategoryControllerTest.java
@@ -1,10 +1,18 @@
 package com.nextgen.categoriesservice.controller;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @WebMvcTest(controllers = {CategoryController.class},
         properties = {
                 "spring.config.import="
         })
 public class CategoryControllerTest {
+
+    @Test
+    void applicationTest() {
+        assertTrue(true);
+    }
 }
